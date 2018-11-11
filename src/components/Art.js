437e75f5f03b0858117cca7b9997e3288b1deba5
geimaj <16000925@vcconnect.co.za>
2018-11-11 11:59:18 +0200

    art functionality

--- src/components/Art.js
+++ src/components/Art.js
@@ -1,37 +1,109 @@
 import React, { Component } from "react";
 import Icon from "./Icon";
+import Animation from "./Animation";
 
 const artData = require("../data/civviesData")
+const watchAnimation = require("../assets/animation/WatchFilm.json")
+const AnimationMouseOn = [0, 12]
+const AnimationMouseOff = [12, 72]
+
 
 class PortfolioItem extends Component {
 
   constructor(props){
     super(props)
 
+    this.artItemClick = this.artItemClick.bind(this)
+    this.imageHoverOn = this.imageHoverOn.bind(this)    
+    this.imageHoverOff = this.imageHoverOff.bind(this)    
+    this.artLinkClicked = this.artLinkClicked.bind(this)    
+    
+    
+
     this.state = {
-      active: 0
+      active: 0,
+      showDetails: false
     }
 
+  }
+
+  artItemClick(key){
+    this.setState({
+      active: key
+    })
+  }
+
+  imageHoverOn(){
+    this.setState({
+      showDetails: true
+    })
+  }
+
+  imageHoverOff(){
+    this.setState({
+      showDetails: false
+    })
+  }
+
+  artLinkClicked(){
+    window.open(artData[this.state.active].link,"_blank")
   }
 
   render() {
 
     let art = artData.map((item, key) => {
+      let cls;
+      if(key === this.state.active){
+        cls = "active"
+      }
       return (
-        <li key={key}>
-          {item.title}
+        <li key={key}
+          className={cls}
+          onClick={() => this.artItemClick(key)}>
+          <h2>
+            {item.title}
+          </h2>
         </li>
       )
     })
 
-    let path = artData[this.state.active].image;
-    // let image = require(path)
-    // console.log(image)
+    let img = artData[this.state.active].image;
 
+    let content;
+
+    if(this.state.showDetails){
+      content = 
+        <div>
+          <p>
+            {artData[this.state.active].description}
+          </p>
+          <p>
+            {artData[this.state.active].type}
+          </p>
+          <p>
+            {artData[this.state.active].by}
+          </p>
+
+          <Animation 
+            mouseOnFrames={AnimationMouseOn}
+            mouseOffFrames={AnimationMouseOff}
+            animationData={watchAnimation}
+            onClick={this.artLinkClicked}/>
+
+        </div>
+    } else {
+      content = 
+      <img  src={`${img}`}
+      />
+    }
 
     return (
       <div className={`page art`}>
-        {/* <img  src={path}/> */}
+        <div className="content"
+          onPointerEnter={this.imageHoverOn}
+          onPointerLeave={this.imageHoverOff}>
+          {content}
+        </div>
         <ul className="artList">
           {art}
         </ul>
--- src/data/civviesData.js
+++ src/data/civviesData.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     title: "Edun 'Karibu'",
-    image: `./assets/civvies/Edun.jpg`,
+    image: require(`../assets/civvies/Edun.jpg`),
     description:
       "Edun is a fashion brand based in New York, who collaborate with artisans across the African continent to produce sustainable, high quality garments. Adriaan Louw directed this mini-documentary, focussing on the brand outlook and the relationships between them and their partners in Nairobi, Kenya. Adriaan did a great job of recording a hand loom, operated by one of the films subjects, which was used as the starting point of the score’s tempo and steady, hypnotic atmosphere. Many cues were also taken from traditional Masaai music and chants.",
     type: "Film Score & Final Mix",
@@ -12,7 +12,7 @@ module.exports = [
 
   {
     title: "Modisakeng - Kin ",
-    image: `./assets/civvies/Kin.jpg`,
+    image: require(`../assets/civvies/Kin.jpg`),
     description:
       "Kin explores the centrality of the concept of kinship to the African lived experience.",
     type: "Sound Design, Original Music & Final Mix",
@@ -24,7 +24,7 @@ module.exports = [
 
   {
     title: "Boiler Room ",
-    image: `./assets/civvies/BoilerRoomA.jpg`,
+    image: require(`../assets/civvies/BoilerRoomA.jpg`),
     description:
       "Breakfast with Snoochie Shy - An episodic interview & variety show by Boiler Room TV.",
     type: "Final Mix",
@@ -35,7 +35,7 @@ module.exports = [
 
   {
     title: "Modisakeng - Zion",
-    image: `./assets/civvies/Zion.jpg`,
+    image: require(`../assets/civvies/Zion.jpg`),
     description:
       "Mohau Modisakeng’s visually striking work is always a pleasure to create immersive sound \
           worlds for. In this instance, there was no sound recordist on set, and every sound needed to be \
@@ -46,7 +46,7 @@ module.exports = [
 
   {
     title: "Over the Rainbow",
-    image: `./assets/civvies/OverTheRainbow.jpg`,
+    image: require(`../assets/civvies/OverTheRainbow.jpg`),
     description:
       "A visual feast, directed by multi-disciplinary artist, Athi Patra-Ruga alongside Jarred Figgins and \
           Jon Kope as DOPs. This film gave our team an opportunity to delve into a surreal, magical and \
@@ -58,7 +58,7 @@ module.exports = [
 
   {
     title: "The African Cypher",
-    image: `./assets/civvies/AfricanCypher.jpg`,
+    image: require(`../assets/civvies/AfricanCypher.jpg`),
     description: `“I dance, as if I have a gun to my head” - Mada Sthembiso, (Shakers&Movers isiPanstula crew). 
            The African Cypher is the birthplace of ritual, celebration, council, storytelling and dance. Street 
           dance is South Africa is a complex, convoluted underworld; that, like most subcultures, exists as 
@@ -74,7 +74,7 @@ module.exports = [
 
   {
     title: "Olifantland",
-    image: `./assets/civvies/Olifantland.jpg`,
+    image: require(`../assets/civvies/Olifantland.jpg`),
     description: `Olifantland, a multilingual visual theatre show featuring five majestic life size elephant puppets by 
       Adrian Kohler from Handspring Puppet Company in collaboration with UKWANDA Puppet and 
       Design Collective. Directed and choreographed by Aja Marneweck. The play, set amidst the 
@@ -92,7 +92,7 @@ module.exports = [
 
   {
     title: "Deep Blue / Middle C",
-    image: `./assets/civvies/DeepBlueMiddleC.jpg`,
+    image: require(`../assets/civvies/DeepBlueMiddleC.jpg`),
     description: `A genre bending documentary film inspired by the west coast of South Africa. “I don’t have a tribal 
       dance, there are no manhood rituals for me. Sitting around this fire with my friends, surfing these 
       West Coast waves, this is my culture.” J’ bones - the dronk kreef. With themes of the ocean, 
@@ -106,7 +106,7 @@ module.exports = [
 
   {
     title: "Modisakeng - Passage",
-    image: `./assets/civvies/Passage.jpg`,
+    image: require(`../assets/civvies/Passage.jpg`),
     description: `Another engrossing work by Mohau Modisakeng, in which we were tasked with creating frantic 
       splashes, whips and creaks for a minimalist piece in which 3 performers (including the artist) are 
       pictured, stranded on sinking life rafts in various phases of submersion. `,
@@ -116,7 +116,7 @@ module.exports = [
 
   {
     title: "Ebisu Drit Matsuri",
-    image: `./assets/civvies/EbisuDrift.jpg`,
+    image: require(`../assets/civvies/EbisuDrift.jpg`),
     description: `Shot and directed by Deon Van Zyl at the Ebisu drift circuit in Japan, he captures the underground and nuanced essence of drift culture.`,
     type: "Original Composition, Sound Design & Final Mix",
     by: "Daniel Lidchi Feat: Alexander Pankiv Greene",
@@ -126,7 +126,7 @@ module.exports = [
 
   {
     title: "Khoi Uprising ",
-    image: `./assets/civvies/KhoiUprising.jpg`,
+    image: require(`../assets/civvies/KhoiUprising.jpg`),
     description: `Praying mantis original people, a short film by Tedeusz Sazcek.`,
     type: "Film Score & Sound Design",
     by: "Simon Kohler"
@@ -134,7 +134,7 @@ module.exports = [
 
   {
     title: "The Mother",
-    image: `./assets/civvies/TheMother.jpg`,
+    image: require(`../assets/civvies/TheMother.jpg`),
     description: `Florian Zeller’s _The Mother_ had its South African premiere at the Fugard Theatre in February 
       2017. The play, directed by Janice Honeyman, dealt with themes of abandonment, separation, 
       and madness. Grim subjects indeed, but fertile ground for a captivating, explorative score. 
@@ -148,7 +148,7 @@ module.exports = [
 
   {
     title: "Modisakeng - Endabeni",
-    image: `./assets/civvies/Endabeni.jpg`,
+    image: require(`../assets/civvies/Endabeni.jpg`),
     description: ` Informed by his experience as a young boy in Soweto, at the crossroads of a violent 
       political transition, Modisakeng uses memory as a portal between past and present to explore 
       themes of history, body and place within the post-apartheid context. His photography, films, 
@@ -161,7 +161,7 @@ module.exports = [
 
   {
     title: "U-SAVED-ME",
-    image: `./assets/civvies/USavedMe.jpg`,
+    image: require(`../assets/civvies/USavedMe.jpg`),
     description: `U-SAVED-ME is an immersive installation combining video, sound, sculpture, tapestry, and 
       drawings. Platter, inspired by the surplus of information and access afforded by portable digital 
       devices and the Internet, includes a representatively disparate sampling of materials: R. Kelly, 
@@ -174,7 +174,7 @@ module.exports = [
 
   {
     title: "OCAL 2016",
-    image: `./assets/civvies/Ocal2016.jpg`,
+    image: require(`../assets/civvies/Ocal2016.jpg`),
     description: `On the 28 April 2016, 12 strangers united on a journey from Cape Town to Pretoria. 
       Over 10 days & nights they would travel 1800km, through the heart of South Africa in a 24-hour 
       non-stop relay. Unified by the common goal to challenge the global belief system around 
@@ -188,7 +188,7 @@ module.exports = [
 
   {
     title: "Endemic Project",
-    image: `./assets/civvies/EndemicProject.jpg`,
+    image: require(`../assets/civvies/EndemicProject.jpg`),
     description: `An immersive installation experience on Rhodes drive, Cape Town South Africa. A future film 
       experiment by filmmaker Bryan Little, featuring a location aware, soundscape score by Simon 
       Kohler.`,
@@ -200,7 +200,7 @@ module.exports = [
 
   {
     title: "Travalien",
-    image: `./assets/civvies/Travailen.jpg`,
+    image: require(`../assets/civvies/Travailen.jpg`),
     description: `At midnight on Monday, March 24, 2014, trailblazers Ryan Sandes and Ryno Griesel embarked 
       on the ultimate Drakensberg adventure, the Drakensberg Grand Traverse, in an attempt to break 
       the current record of 60h 29m. This keenly observant, 27 minute film, directed by Dean Leslie, 
