93f89c69e3bebdfc6eab6c34b49dc5623a1156c8
geimaj <16000925@vcconnect.co.za>
2018-11-11 13:10:15 +0200

    team functionality

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,14 +1,117 @@
 import React, { Component } from "react";
 import Icon from "./Icon"
+import Animation from "./Animation"
+
+const simon = require('../assets/animation/Simon.json');
+const nic = require('../assets/animation/Nicolaas.json');
+const daniel = require('../assets/animation/Daniel.json');
+const dylan = require('../assets/animation/Dylan.json');
+
+const AnimationMouseOn = [0, 12]
+const AnimationMouseOff = [12, 72]
+
+const teamData = require("../data/teamData")
+
 
 class Team extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.handleNameHover = this.handleNameHover.bind(this)
+    this.teamNameClick = this.teamNameClick.bind(this)
+    this.exitClick = this.exitClick.bind(this)
+    
+
+    this.state = {
+      active: 0,
+      showInfo: false
+    }
+
+  }
+
+  handleNameHover(key) {
+    if(!this.state.showInfo){
+
+      this.setState({
+        active: key
+      })
+    }
+  }
+
+  teamNameClick(){
+    this.setState({
+      showInfo: true
+    })
+  }
+
+  exitClick(){
+    this.setState({
+      showInfo: false
+    })
+  }
+
+  teamNames(){
+    let team = teamData.map((item, key) => {
+      let cross;
+      if(key === this.state.active && this.state.showInfo){
+        cross = 
+          <Icon
+            className="exit"
+            onClick={this.exitClick}
+            src={require('../assets/icon/Close.svg')} />
+      }
+      return (
+        <div onPointerEnter={()=> this.handleNameHover(key)} key={key}>
+              <Animation
+                className="teamAnimation"
+                mouseOnFrames={AnimationMouseOn}
+                mouseOffFrames={AnimationMouseOff}
+                animationData={item.animationData}
+                onClick={this.teamNameClick} />
+
+                {cross}
+
+        </div>
+      )
+    })
+
+    return team;
+  }
+
   render() {
+
+    let content;
+    let roles = teamData[this.state.active].roles.map((item) => {
+      return <li><h2>{item}</h2></li>
+    })
+
+
+    if(this.state.showInfo){
+      content = 
+        <div className="details">
+        <ul>
+          {roles}
+        </ul>
+          {teamData[this.state.active].description}
+        </div>
+    } else {
+      content = 
+        <img src={teamData[this.state.active].image} />
+        
+    }
+
     return (
-      <div className="page">
-        <h2>Team</h2>
+      <div className="page team">
+        <div className="names">
+          {this.teamNames()}
+        </div>
+        <div className="content">
+          {content}
+        </div>
       </div>
     );
   }
 }
- 
+
 export default Team;
---
+++ src/data/teamData.js
@@ -0,0 +1,46 @@
+module.exports = [
+    {
+        roles: ["composer", "co-founder"],
+        description: `After spending too much time in India, Si headed back to the Cape coastline, only to land a seat 
+            in the illustrious film gang; FLYONTHEWALL Productions. Until this point, a musical life was but 
+            a dream and so he hastily set about learning the craft of sound and story; quickly becoming 
+            FOTW's in-house composer. Fast forward 7 years, Si now co-pilots the Field mother ship 
+            alongside his impressive comrades. Celebrated for his ability to weave tapestries of sound, 
+            Simon is applauded for his deep sensitivities, diplomacy and gentle nature; his beats though, are 
+            big as f%&amp;k.`,
+        image: require('../assets/team/Simon.jpg'),
+        animationData: require('../assets/animation/Simon.json'),
+        name: "Simon Kohler"
+    },
+    {
+        roles: ["composer", "co-founder"],
+        description: `Nic van Reenen spent most of his teenage years maddening the staff at the neighbouring 
+            doctor’s practice with musical noise of all varieties and timbres emanating from his bedroom. 
+            He’s grown to become rather capable of restraining and coaxing the chaotic noise into 
+            compelling and moving musical forms. Nic has a varied and eclectic creative musical resume, 
+            having worked with projects such as Bateleur, Spoek Mathambo, Follower, Petit Noir, Bongeziwe 
+            Mabandla, Fever Trails, Ex Olympic &amp; more.`,
+        image: require('../assets/team/Nicolaas.jpg'),
+        animationData: require('../assets/animation/Nicolaas.json'),
+        name: "Nicolaas Van Reenen"    
+    },
+    {
+        roles: ["composer"],
+        description: `When not contemplating which USN product is best for his muscle growth, visiting the local 
+            organic market or losing money trying to short Bitcoin - you'll most likely find him stressing over 
+            how to get "PERFECT ROOM ACOUSTICS" or DJ'ing in dungeons at hours Helen Zille would 
+            frown upon. He is also a co-founder and mixing engineer of the record label '1991', and produces 
+            music under Maxime Alexander`,
+        image: require('../assets/team/Daniel.jpg'),
+        animationData: require('../assets/animation/Daniel.json'),
+        name: "Daniel Lidchi"
+    }, 
+    {
+        roles: ["designer"],
+        description: `At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident
+        `,
+        image: require('../assets/team/Dylan.jpg'),
+        animationData: require('../assets/animation/Dylan.json'),
+        name: "Dylan Godfrey"
+    }
+]
