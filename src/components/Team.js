8df5cb3a4c33809793aad76a3eb0d481b1f6ad40
geimaj <16000925@vcconnect.co.za>
2018-11-12 19:44:46 +0200

    team fades and styles

--- src/components/Team.js
+++ src/components/Team.js
@@ -5,7 +5,15 @@ import Animation from "./Animation"
 const AnimationMouseOn = [0, 12]
 const AnimationMouseOff = [12, 72]
 
+const $ = require('jquery')
+
 const teamData = require("../data/teamData")
+const viewBox = {
+  x: 0,
+  y: 35,
+  w: 300,
+  h: 100
+}
 
 
 class Team extends Component {
@@ -16,56 +24,92 @@ class Team extends Component {
     this.handleNameHover = this.handleNameHover.bind(this)
     this.teamNameClick = this.teamNameClick.bind(this)
     this.exitClick = this.exitClick.bind(this)
-    
 
     this.state = {
-      active: 0,
       showInfo: false
     }
 
+    this.content = teamData.map((item, key) => {
+      let roles = teamData[key].roles.map((item1, key1) => {
+        return <li key={key1}><h2>{item1}</h2></li>
+      })
+      return (
+
+        <div className="teamContent" key={key}>
+          <img src={item.image} alt={item.name} />
+          <div className="details">
+            <ul>
+              {roles}
+            </ul>
+            <div>
+              {item.description}
+            </div>
+          </div>
+        </div>
+      )
+    })
+
+  }
+
+  componentDidMount() {
+    $(".teamContent").eq(0).addClass('active')
   }
 
   handleNameHover(key) {
-    if(!this.state.showInfo){
+    if (!this.state.showInfo) {
 
-      this.setState({
-        active: key
-      })
+      $(".teamContent").removeClass('active')
+      $(".teamContent").eq(key).addClass('active')
+
     }
   }
 
-  teamNameClick(){
+  teamNameClick(key) {
+    if (!this.state.showInfo) {
+    
+    $(".teamContent").removeClass('showDetails')
+    $(".teamContent").eq(key).addClass('showDetails')
+
+    $(".exit").removeClass("active")
+    $(".exit").eq(key).addClass('active')
+
     this.setState({
       showInfo: true
     })
   }
 
-  exitClick(){
+  }
+
+  exitClick() {
+    $(".teamContent").removeClass('showDetails')
+    $(".exit").removeClass("active")
+
+
+
     this.setState({
       showInfo: false
     })
   }
 
-  teamNames(){
+  teamNames() {
     let team = teamData.map((item, key) => {
       let cross;
-      if(key === this.state.active && this.state.showInfo){
-        cross = 
-          <Icon
-            className="exit"
-            onClick={this.exitClick}
-            src={require('../assets/icon/Close.svg')} />
-      }
+      cross =
+        <Icon
+          className="exit"
+          onClick={this.exitClick}
+          src={require('../assets/icon/Close.svg')} />
       return (
-        <div onPointerEnter={()=> this.handleNameHover(key)} key={key}>
-              <Animation
-                className="teamAnimation"
-                mouseOnFrames={AnimationMouseOn}
-                mouseOffFrames={AnimationMouseOff}
-                animationData={item.animationData}
-                onClick={this.teamNameClick} />
+        <div onPointerEnter={() => this.handleNameHover(key)} key={key}>
+          <Animation
+            className="teamAnimation"
+            mouseOnFrames={AnimationMouseOn}
+            mouseOffFrames={AnimationMouseOff}
+            animationData={item.animationData}
+            viewBox={viewBox}
+            onClick={() => this.teamNameClick(key)} />
 
-                {cross}
+          {cross}
 
         </div>
       )
@@ -76,24 +120,24 @@ class Team extends Component {
 
   render() {
 
-    let content;
-    let roles = teamData[this.state.active].roles.map((item, key) => {
-      return <li key={key}><h2>{item}</h2></li>
-    })
+    // let content;
+    // let roles = teamData[this.state.active].roles.map((item, key) => {
+    //   return <li key={key}><h2>{item}</h2></li>
+    // })
 
-    if(this.state.showInfo){
-      content = 
-        <div className="details">
-        <ul>
-          {roles}
-        </ul>
-          {teamData[this.state.active].description}
-        </div>
-    } else {
-      content = 
-        <img src={teamData[this.state.active].image} alt={teamData[this.state.active].name} />
-        
-    }
+    // if(this.state.showInfo){
+    //   content = 
+    //     <div className="details">
+    //     <ul>
+    //       {roles}
+    //     </ul>
+    //       {teamData[this.state.active].description}
+    //     </div>
+    // } else {
+    //   content = 
+    //     <img src={teamData[this.state.active].image} alt={teamData[this.state.active].name} />
+
+    // }
 
     return (
       <div className="page team">
@@ -101,7 +145,7 @@ class Team extends Component {
           {this.teamNames()}
         </div>
         <div className="content">
-          {content}
+          {this.content}
         </div>
       </div>
     );
