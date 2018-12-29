b2e28099f3cb5c4bff96d309395f4a03b473aa42
geimaj <16000925@vcconnect.co.za>
2018-12-29 17:32:40 +0200

    Team - Fade between images broken

--- src/components/Team.js
+++ src/components/Team.js
@@ -94,10 +94,21 @@ class Team extends Component {
 
   render() {
 
-    let item = teamData[this.state.activeIndex]
+    // <TeamMemberContent 
+    // key={key}
+    // item={teamData[this.state.activeIndex]}
+    // showInfo={this.state.showInfo} />
 
-    let content =
-      <TeamMemberContent item={teamData[this.state.activeIndex]} showInfo={this.state.showInfo} />
+    let content = teamData.map((item, key) => {
+    return (
+      <TeamMemberContent 
+        key={key}
+        item={item}
+        active={this.state.activeIndex == key}
+        showInfo={this.state.showInfo} />
+    ) 
+
+    })
 
     return (
       <div className="page team">
